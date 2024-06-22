dbac67489610478b6b2e3d83129c9be535f5d1e5
ThomasHeideman <47209974+ThomasHeideman@users.noreply.github.com>
2024-06-22 11:57:26 +0200

    finished project

--- script.js
+++ script.js
@@ -1,1 +1,80 @@
 'use strict';
+const postContainer = document.getElementById('post-container');
+const filter = document.getElementById('filter');
+const loading = document.getElementById('loader');
+
+let limit = 3;
+let page = 1;
+
+// fetch posts from API
+async function getPosts() {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+  );
+
+  const data = await res.json();
+  return data;
+}
+
+// show posts in DOM
+async function showPosts() {
+  const posts = await getPosts();
+  posts.forEach(post => {
+    const postEl = document.createElement('div');
+    postEl.className = 'post';
+
+    postEl.innerHTML = `
+     <div class="number">${post.id}</div>
+        <div class="post-info">
+          <h2 class="post-title">${post.title}</h2>
+          <p class="post-body">
+            ${post.body}
+          </p>
+    `;
+    postContainer.appendChild(postEl);
+  });
+}
+
+// show loader and fetch more posts
+function showLoading() {
+  loading.classList.add('show');
+  setTimeout(() => {
+    loading.classList.remove('show');
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 200);
+  }, 400);
+}
+
+// filter posts by input
+
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll('.post');
+  console.log(term);
+
+  posts.forEach(post => {
+    const title = post.querySelector('.post-title').innerText.toUpperCase();
+    const body = post.querySelector('.post-body').innerText.toUpperCase();
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = 'flex';
+    } else {
+      post.style.display = 'none';
+    }
+  });
+}
+
+// show initial posts
+showPosts();
+
+//  add posts on scroll
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
+
+filter.addEventListener('input', filterPosts);
